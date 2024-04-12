3455a07daa9f45aa0dd6fb9c5dda543fcf57f37f
Nicolas Bottarini <nicolasbottarini@gmail.com>
2024-04-12 11:14:27 -0300

    set minimum jdk version to 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,22 @@
 import com.vanniktech.maven.publish.SonatypeHost
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.9.23"
     id("com.vanniktech.maven.publish") version "0.28.0"
-    id("dev.botta.kotlin-conventions") version "0.1.0"
+    id("dev.botta.kotlin-conventions") version "0.2.0"
 }
 
 group = "dev.botta"
-version = "1.0.1"
+version = "1.1.0"
+
+repositories {
+    mavenCentral()
+}
 
 dependencies {
     implementation(kotlin("stdlib"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
     testImplementation("org.assertj:assertj-core:3.25.3")
-}
-
-tasks.withType<KotlinCompile> { kotlinOptions.jvmTarget = "1.8" }
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 mavenPublishing {

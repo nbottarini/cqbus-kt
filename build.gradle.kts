a8e277f7d6eeca7645941c2b5a708c6db15139b4
Nicolas Bottarini <nicolasbottarini@gmail.com>
2024-04-12 09:56:34 -0300

    use new kotlin conventions gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,59 +4,29 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.9.23"
     id("com.vanniktech.maven.publish") version "0.28.0"
+    id("dev.botta.kotlin-conventions") version "0.1.0"
 }
 
 group = "dev.botta"
-version = "1.0.0"
-
-repositories {
-    mavenCentral()
-}
+version = "1.0.1"
 
 dependencies {
     implementation(kotlin("stdlib"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
-    testImplementation("org.assertj:assertj-core:3.25.2")
+    testImplementation("org.assertj:assertj-core:3.25.3")
 }
 
 tasks.withType<KotlinCompile> { kotlinOptions.jvmTarget = "1.8" }
 
-kotlin {
-    sourceSets["main"].apply {
-        kotlin.srcDirs("src")
-        resources.srcDirs("resources")
-    }
-    sourceSets["test"].apply {
-        kotlin.srcDir("test")
-        resources.srcDir("test_resources")
-    }
-}
-
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
-
-    sourceSets["main"].apply {
-        java.srcDirs("src")
-        resources.srcDirs("resources")
-    }
-    sourceSets["test"].apply {
-        java.srcDir("test")
-        resources.srcDir("test_resources")
-    }
-}
-
-tasks.withType<Test> { useJUnitPlatform() }
-
-tasks.withType<Test>().configureEach {
-    reports.html.required.set(false)
-    reports.junitXml.required.set(false)
 }
 
 mavenPublishing {
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
     signAllPublications()
-    coordinates("dev.botta", "cqbus", "1.0.0")
+    coordinates(project.group.toString(), "cqbus", project.version.toString())
 
     pom {
         name.set("CQBus")
